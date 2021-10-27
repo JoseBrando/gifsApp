6baf2cd94a15657e7d601069a6100ba20caa2dad
Brando <jose.brando.c.m@gmail.com>
2021-10-27 13:41:55 -0500

    [change] search "ViewChild"

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,9 +6,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  @ViewChild('txtSearch') txtSearch!: ElementRef<HTMLInputElement>; // !: operador para indicar que el elemento no es nulo
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public search(termino: string): void {
+    const VALOR = this.txtSearch.nativeElement.value;
+
+    console.log(VALOR);
+
+    this.txtSearch.nativeElement.value = '';
   }
 
 }
