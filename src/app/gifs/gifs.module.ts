b525d42b334e61483fdcf3d3e7495b3d84aa6dde
Brando <jose.brando.c.m@gmail.com>
2021-10-27 11:30:34 -0500

    [ADD] gifsPage component

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
+// Components
+import { GifsPageComponent } from './gifs-page/gifs-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GifsPageComponent
+  ],
+  exports: [
+    GifsPageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class GifsModule { }
