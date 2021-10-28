943745238a2f6ed04e4c37fa03faf95e106f5806
Brando <jose.brando.c.m@gmail.com>
2021-10-27 22:17:06 -0500

    API giphy

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,17 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable, Query } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root' // Servicio global en el root (El servicio puede usarse en cualquier parte de la app)
 })
 export class GifsService {
 
+  private apiKey: string = 'RfklaJpNBEUX5jVpw6PJFuVkwXJlz38D';
   private _historial: string[] = [];
 
   get historial() {
     return [...this._historial];
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   buscarGifs(query: string = '') {
 
@@ -21,5 +24,11 @@ export class GifsService {
       this._historial.unshift( query );
       this._historial = this._historial.splice(0, 10);
     }
+
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=RfklaJpNBEUX5jVpw6PJFuVkwXJlz38D&q=hi&limit=5')
+    .subscribe(
+      (response: any) => {
+      console.log(response);
+    });
   }
 }
